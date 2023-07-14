8a2d1a1e3e993f8257b032df71176d23645e4b49
ciprizare <cipri.zare@gmail.com>
2023-07-14 10:38:37 +0300

    Add product endpoints

--- WepApi/Controllers/ProductController.cs
+++ WepApi/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Services.Interfaces;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WepApi.Controllers
@@ -21,12 +22,72 @@ namespace WepApi.Controllers
         public async Task<IActionResult> GetAllProducts()
         {
             var products = await _service.GetAllProducts();
-            if (products == null) 
+            if (products == null || !products.Any()) 
             {
                 return NotFound();
             }
             return Ok(products);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _service.GetProduct(id);
+            if (product == null) 
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            var newProduct = await _service.AddProduct(product);
+            if (!newProduct)
+                return BadRequest();
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] int id, [FromBody] Product product)
+        {
+            var updatedProd = await _service.GetProduct(id);
+            if (updatedProd == null) 
+            {
+                return NotFound();
+            }
+
+            updatedProd.Name = product.Name;
+            updatedProd.Description = product.Description;
+            updatedProd.Price = product.Price;
+            updatedProd.PictureUrl = product.PictureUrl;
+            updatedProd.QuantityInStock = product.QuantityInStock;
+            updatedProd.Brand = product.Brand;
+            updatedProd.Type = product.Type;
+
+            var updateProduct = await _service.UpdateProduct(updatedProd);
+
+            if (!updateProduct)
+                return BadRequest();
+
+            return Ok(updatedProd);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveProduct(int id)
+        {
+            var product = await _service.GetProduct(id);
+            if (product == null)
+                return NotFound();
+            var removedProduct = await _service.RemoveProduct(product);
+
+            if (!removedProduct) 
+                return BadRequest();
+
+            return Ok();
+        }
+
     }
 
 }
